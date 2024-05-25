a9f1378257701352e77384466634627356bd235b
EricZeller <98104805+EricZeller@users.noreply.github.com>
2024-05-25 21:58:05 +0200

    location updated

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -12,7 +12,11 @@ class _ChooseLocationState extends State<ChooseLocation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Choose a location"),
+        title: Text(
+          "Choose a location",
+          style: TextStyle(
+              fontFamily: "Red Hat Display", fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
       ),
       body: Text("Location choser"),
