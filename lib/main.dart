0fa6a6113cee4a69a2a5b0e1a915a68188a90f9b
EricZeller <98104805+EricZeller@users.noreply.github.com>
2024-05-25 21:43:22 +0200

    init + fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:world_clock/pages/home.dart';
+import 'package:world_clock/pages/loadingscreen.dart';
+import 'package:world_clock/pages/location.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => LoadingScreen(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation()
+  },
+));
